3382381aee0a162a7dd28ef17701efab00707688
Anthony Pipia <apipia@starbucks.com>
2019-12-10 13:05:02 -0800

    Updated class name of new blackduck hub test to avoid conflicts

--- dojo/unittests/test_blackduck_v2_zip_parser.py
+++ dojo/unittests/test_blackduck_v2_zip_parser.py
@@ -4,7 +4,7 @@ from dojo.models import Test
 from pathlib import Path
 
 
-class TestBlackduckHubParser(TestCase):
+class TestBlackduckHubV2Parser(TestCase):
     def test_blackduck_enhanced_zip_upload(self):
         testfile = Path("dojo/unittests/scans/blackduck_V2/blackduck_hub_v2.zip")
         parser = BlackduckHubCSVParser(testfile, Test())
